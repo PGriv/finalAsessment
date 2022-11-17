d0ce034798592d8653fe4a69c4389f98ed677ec5
PGriv <petrosgrivas1996@gmail.com>
2022-11-17 09:51:45 +0100

    Initial commit

---
+++ src/main/java/com/finalAsessment/finalAsessment/FinalAsessmentApplication.java
@@ -0,0 +1,13 @@
+package com.finalAsessment.finalAsessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAsessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalAsessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalAsessment/finalAsessment/FinalAsessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalAsessment.finalAsessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAsessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
